803f98dbbdec086918a38d08a7d9b74906a05f1a
Alexander Bal <alexander.bal@student.hogent.be>
2024-06-02 18:48:37 +0200

    added when clause on windowsize in mainactivity

--- app/src/main/java/com/example/skysnap/MainActivity.kt
+++ app/src/main/java/com/example/skysnap/MainActivity.kt
@@ -6,18 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.skysnap.ui.theme.SkySnapTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,6 +38,21 @@ class MainActivity : ComponentActivity() {
                         contentScale = ContentScale.FillBounds,
                         modifier = Modifier.fillMaxSize()
                     )
+
+                    when (windowSize.widthSizeClass) {
+                        WindowWidthSizeClass.Compact -> {
+
+                        }
+                        WindowWidthSizeClass.Medium -> {
+
+                        }
+                        WindowWidthSizeClass.Expanded -> {
+
+                        }
+                        else -> {
+
+                        }
+                    }
                 }
             }
         }
