24d6cf4baca03b10fe8b8b975ad8e0bb6fc66679
Alexander Bal <alexander.bal@student.hogent.be>
2024-06-02 18:55:06 +0200

    added preview of skySnapApp composable

--- app/src/main/java/com/example/skysnap/ui/SkySnapApp.kt
+++ app/src/main/java/com/example/skysnap/ui/SkySnapApp.kt
@@ -1,7 +1,17 @@
 package com.example.skysnap.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.skysnap.ui.theme.SkySnapTheme
 
 @Composable
 fun skySnapApp() {
+}
+
+@Preview(showBackground = true)
+@Composable
+fun skySnapAppPreview() {
+    SkySnapTheme {
+        skySnapApp()
+    }
 }
