0b103bbae093731a96388872c5a84028dcaafec4
Alexander Bal <alexander.bal@student.hogent.be>
2024-06-13 17:03:43 +0200

    added color theme

--- app/src/main/java/com/example/skysnap/ui/theme/Color.kt
+++ app/src/main/java/com/example/skysnap/ui/theme/Color.kt
@@ -1,11 +1,16 @@
 package com.example.skysnap.ui.theme
-
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val primaryLight = Color(0xFF000000)
+val secondaryLight = Color(0xFF3C3C43)
+val tertiaryLight = Color(0x4D3C3C43)
+val quaternaryLight = Color(0x2E3C3C43)
+val primaryDark = Color(0xFFFFFFFF)
+val secondaryDark = Color(0xFFEBEBF5)
+val tertiaryDark = Color(0xFFEBEBF5)
+val quaternaryDark = Color(0xFFEBEBF5)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val solid1 = Color(0xFF48319D)
+val solid2 = Color(0xFF1F1D47)
+val solid3 = Color(0xFFC427FB)
+val solid4 = Color(0xFFE0D9FF)
