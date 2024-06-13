98f954f0387b7fcbad9b150d813267ecc78cffa5
Alexander Bal <alexander.bal@student.hogent.be>
2024-06-13 17:12:23 +0200

    added Home and Favourites to OverviewScreens

---
+++ app/src/main/java/com/example/skysnap/navigation/enums/OverviewScreens.kt
@@ -0,0 +1,13 @@
+package com.example.skysnap.navigation.enums
+
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.skysnap.R
+
+enum class OverviewScreens(@StringRes val title: Int, val icon: ImageVector) {
+    Home(title = R.string.home, icon = Icons.Filled.Home),
+    Favourites(title = R.string.favourites, icon = Icons.Filled.Favorite)
+}
